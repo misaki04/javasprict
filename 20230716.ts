23109ac864ac669ded2b5e5d735926edebe69195
misaki04 <misakichi0043@gmail.com>
2023-07-16 23:58:26 +0900

    type Script変数とデータの方法について

---
+++ 20230716.ts
@@ -0,0 +1,21 @@
+// 変数とデータ型を学ぶ
+// 変数宣言
+let price = 120;
+let input = 200;
+let change = input - price;
+console.log(change);
+
+// 定数
+const price1 = 120;
+price1 = 130;
+// priceは再代入できない
+let quantity = 2;
+quantity = 1;
+// letの再代入でletをつけるとエラー
+
+// データ型
+// number型(数値型)
+120 - // 整数
+  0.5; //　少数
+
+999_999_999; //999999999同じ
